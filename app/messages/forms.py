72e6e755082e80a91d3484a561f03cdb8595e700
Louis Becker <LBecker@meatco.com.na>
2020-03-01 18:15:28 +0200

    Fixed some errors and added a proper editor to the site.

--- app/messages/forms.py
+++ app/messages/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, SubmitField, validators, TextAreaField, BooleanField, HiddenField)
+from wtforms import (StringField, SubmitField, validators, BooleanField, HiddenField, TextAreaField)
 
 
 class MessageForm(FlaskForm):
-    is_urgent = BooleanField('Send as urgent broadcast')
+    is_urgent = BooleanField('Notify everyone about the new message by SMS and/or e-mail')
     user_list = HiddenField('User List')
     subject = StringField('Subject', validators=[validators.DataRequired()])
-    content = TextAreaField('Content', validators=[validators.DataRequired()])
+    content = TextAreaField('Content', validators=[validators.DataRequired()], )
     submit = SubmitField('Submit Message')
 
